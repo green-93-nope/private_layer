d05befc239025b23441c9d3b40c35777e9baacc6
Green <854232507@qq.com>
2017-04-25 14:59:02 +0800

    add external program with mpv and libreoffice

--- green-tool/packages.el
+++ green-tool/packages.el
@@ -62,7 +62,26 @@ Each entry is either:
   (use-package openwith
     :init
     (openwith-mode t)
-    (setq openwith-associations '(("\\.pdf\\'" "okular" (file)))))
+    (setq openwith-associations
+          (list
+           (list (openwith-make-extension-regexp
+                  '("mpg" "mpeg" "mp3" "mp4"
+                    "avi" "wmv" "wav" "mov" "flv"
+                    "ogm" "ogg" "mkv"))
+                 "mpv"
+                 '(file))
+           (list (openwith-make-extension-regexp
+                  '("doc" "xls" "ppt" "odt" "ods" "odg" "odp"))
+                 "libreoffice"
+                 '(file))
+           '("\\.lyx" "lyx" (file))
+           '("\\.chm" "kchmviewer" (file))
+           (list (openwith-make-extension-regexp
+                  '("pdf" "ps" "ps.gz" "dvi"))
+                 "okular"
+                 '(file))
+           ))
+    )
   )
 
 ;;; packages.el ends here
